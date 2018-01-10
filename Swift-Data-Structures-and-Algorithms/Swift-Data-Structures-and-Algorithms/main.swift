a2b2a9412c95b3333baf8282c783c36bb9bcb942
Pavel Palancica <pavel.palancica@gmail.com>
2018-01-09 18:09:18 -0600

    First commit

---
+++ Swift-Data-Structures-and-Algorithms/Swift-Data-Structures-and-Algorithms/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Data-Structures-and-Algorithms
+//
+//  Created by Pavel Palancica on 1/9/18.
+//  Copyright © 2018 I Dev Wizard. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
